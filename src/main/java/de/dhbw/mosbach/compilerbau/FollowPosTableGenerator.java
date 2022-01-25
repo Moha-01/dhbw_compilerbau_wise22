c2b5e634dbb64d1b949d6e3f50984efd435bd57d
Lukas <75485295+1ukas31@users.noreply.github.com>
2022-01-25 15:54:40 +0100

    Codeskelett
    Codeskelett von folgenden Interfaces:
    - Visitor
    - Visitable
    
    Codeskelett
    - Visitor2

---
+++ src/main/java/de/dhbw/mosbach/compilerbau/FollowPosTableGenerator.java
@@ -0,0 +1,15 @@
+public class FollowPosTableGenerator implements Visitor{
+
+  public void visit(OperandNode node){
+    //...
+  }
+
+  public void visit(BinOpNode node){
+    //...
+  }
+
+  public void visit(UnaryOpNode node){
+    //...
+  }
+
+}
