77c16df72055c1f3293e64415dcc65488556c873
Lukas <75485295+1ukas31@users.noreply.github.com>
2022-01-30 21:40:01 +0100

    FollowPosTableGenerator

--- src/main/java/de/dhbw/mosbach/compilerbau/FollowPosTableGenerator.java
+++ src/main/java/de/dhbw/mosbach/compilerbau/FollowPosTableGenerator.java
@@ -1,22 +1,51 @@
 /*
-*
-* */
+  Author:    Lukas
+  Created:   DD.MM.YYYY
+  Edited From: Lukas, 30.01.2022
+*/
 
 package de.dhbw.mosbach.compilerbau;
 
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class FollowPosTableGenerator implements Visitor{
 
+  private String konkatenation = "°";
+  private String kleenscheHuelle = "*";
+  private String postitiveHuelle = "+";
 
-  public void visit(OperandNode node){
-    //...
+
+  public SortedMap<Integer, FollowPosTableEntry> followPosTableEntries = new TreeMap<>();
+
+  /**
+   * Neue Zeile in Tabelle anlegen
+   * new FollowPosTableEntry mit Position, Symbol und leeres HashSet<Integer>
+   * @param node Blattknoten
+   */
+  public void visit(OperandNode node){    //Blattknoten
+    followPosTableEntries.put(node.position, new FollowPosTableEntry(node.position, node.symbol));    //Eintrag in Tabelle
   }
 
-  public void visit(BinOpNode node){
-    //...
+  public void visit(BinOpNode node){      //innerer Knoten
+    if(node.operator == konkatenation){   //checken ob es sich beim operator um eine Konkatenation handelt
+      SyntaxNode left = (SyntaxNode) node.left;
+      SyntaxNode right = (SyntaxNode) node.right;
+      for(int i : left.lastpos){          //iterieren über jede Position i von der lastpos Menge der linken SyntaxNode
+        followPosTableEntries.get(i).followpos.addAll(right.firstpos);    //followpos(Position i) = followpos(Position i) ODER firstpos(rechter Sohnknoten) für alle i
+      }
+    }
+    else{
+      //keine Konkatenation
+    }
   }
 
-  public void visit(UnaryOpNode node){    //star-node
-    //...
+  public void visit(UnaryOpNode node){    //KleenscheHuelle und PositiveHuelle
+    if(node.operator == kleenscheHuelle || node.operator == postitiveHuelle){
+      for(int i : node.lastpos){
+        followPosTableEntries.get(i).followpos.addAll(node.firstpos);
+      }
+    }
   }
 
 }
